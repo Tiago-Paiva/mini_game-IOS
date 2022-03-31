0be8860c1e6f4e564835cc8eaf30454bca4cf74b
tiagopaiva-prjfinal <tiagogames21@gmail.com>
2022-03-31 01:02:21 +0100

    create the first forms - Memorize
    create the first forms - Memorize

---
+++ memorize/memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tiago Paiva on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
